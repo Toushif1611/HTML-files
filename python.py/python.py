6c75e828066ac794da4f5925ea39429173b0e1b8
toushifA <toushif1611@gamil.com>
2023-11-08 20:00:53 +0530

    create border and turtle motion

--- python.py/python.py
+++ python.py/python.py
@@ -1,4 +1,7 @@
 import turtle
+
+speed=1
+
 s=turtle.Screen()
 s.bgcolor("black")
 
@@ -7,7 +10,17 @@ a.color("blue")
 a.penup()
 a.speed(0)
 
-speed=1
+b=turtle.Turtle()
+b.color("green")
+b.pensize(5)
+b.speed(0)
+b.penup()
+b.goto(-300,-300)
+b.pendown()
+for i in range(4):
+    b.forward(600)
+    b.left(90)
+    b.hideturtle()
 
 def turnleft():
     a.left(30)
@@ -20,7 +33,7 @@ s.onkeypress(turnleft,"Left")
 s.onkeypress(turnright,"Right")
 
 while True:
-    a.forward("speed")
+    a.forward(speed)
 
     
 turtle.mainloop()
